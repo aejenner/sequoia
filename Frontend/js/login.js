c947619e7adc2ab15c15f30df9a659dd7960250f
Steven Bui <bui.steven.d@gmail.com>
2013-12-04 16:08:26 -0800

    add to login/register

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -4,16 +4,40 @@ $(document).ready(function(){
 	$("#toRegister").click(function(){
 		//hide login div, show register
 		$('#login').css({'display':'none'});
-		$('#register').show("blind",650);
+		$('#register').show("blind",800);
 		//$('#register').css({'display':'block'});
 	});
 	
 	$("#toLogin").click(function(){
 		//hide register show login
 		$('#register').css({'display':'none'});
-		$('#login').show("blind",650);
+		$('#login').show("blind",800);
 		//$('#login').css({'display':'block'});
 	});
+	
+	$(".showpassword").each(function (index, input) {
+    var $input = $(input);
+    $("button").click(function () {
+      var change = "";
+      if ($(this).html() === "Show Password") {
+        $(this).html("Hide Password")
+        change = "text";
+      } else {
+        $(this).html("Show Password");
+        change = "password";
+      }
+      var rep = $("<input type='" + change + "' />")
+        .attr("id", $input.attr("id"))
+        .attr("name", $input.attr("name"))
+        .attr('class', $input.attr('class'))
+		.attr("required", $input.attr('required'))
+        .val($input.val())
+        .insertBefore($input);
+      $input.remove();
+      $input = rep;
+    }).insertAfter($input);
+	});
+	
  
  
  
